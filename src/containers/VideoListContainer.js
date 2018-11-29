e331f7012df892d46ad11bbd3be7fb016ac7d659
danieljhegeman <student@hackreactor.com>
2018-11-29 10:21:38 -0800

    Revert "Container Finished"
    This reverts commit 86259c85b613d124ad46543d91bd47de25e51140.

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,21 +2,7 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
-
-const mapStateToProps = (state) => (
-    {videos: state.videoList}
-)
-
-const mapDispatchToProps = dispatch => {
-    return {
-        handleVideoListEntryTitleClick: video => dispatch(changeVideo(video))
-    }
-}
-
-var VideoListContainer = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(VideoList)
+var VideoListContainer = () => {};
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
